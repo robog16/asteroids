010b3f03bfc3a06ff2a3762c080ac5618242498c
robog16 <robo.gombar@gmail.com>
2025-05-05 22:37:22 +0200

    moving throughout screen enabled

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     # Keep your existing print statements
@@ -10,21 +11,34 @@ def main():
     # Initialize pygame
     pygame.init()
     
+    time_clock = pygame.time.Clock()
+    dt = 0
+
     # Create the game window
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    # In your main function, instantiate a Player object
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     
     # Your game loop
-    while True:
+    running = True
+    while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                return
+                running = False
         
         # Fill the screen with black
         screen.fill('black')
+        player.update(dt)
+        
+        # re-render the player on the screen each frame
+        player.draw(screen)
 
         # Update the display to show the changes
         pygame.display.flip()
 
+        # At the end of each loop iteration, call tick on the time_clock object
+        dt = time_clock.tick(60)/1000
 
 if __name__ == "__main__":
     main()
